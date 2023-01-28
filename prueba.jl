e7e477f5371f63ac87ea3023d1813f63c2ed8d29
Alfredo Sánchez Alberca <asalber@gmail.com>
2023-01-28 13:35:55 +0100

    Añadida solucion examen 2022-12-14.

--- prueba.jl
+++ prueba.jl
@@ -1,2 +1,16 @@
-f(x)=
-print(f)
+using Plots, SymPy
+@vars x real=true
+# f(x) = ℯ^(atan(x/2))
+# plot(f)
+# solve(diff(f,2))
+
+
+# x(n) = n == 1 ? 2 : sqrt(x(n-1)/2)
+# sum([x(n) for n = 1:50])
+
+#f(x) = tan(x)-1/(cos(x)-sin(x))
+#limit(f(x), x=>Sym(pi)/4)
+
+f(x) = x^3/(x-1)^2
+limit(f(x)/x, x=>oo)
+limit(f(x)-x, x=>oo)
