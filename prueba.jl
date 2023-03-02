488d4547db4e84eef4c1d6e27313d56f6d4cca57
Alfredo Sánchez Alberca <asalber@gmail.com>
2023-03-02 01:46:40 +0100

    Añadida práctica de series.

--- prueba.jl
+++ prueba.jl
@@ -4,13 +4,20 @@ using Plots, SymPy
 # plot(f)
 # solve(diff(f,2))
 
+f(x) = sin(2^x)
+N(series(f(x), x, 0, 12).removeO()(1/2))
 
-# x(n) = n == 1 ? 2 : sqrt(x(n-1)/2)
-# sum([x(n) for n = 1:50])
 
-#f(x) = tan(x)-1/(cos(x)-sin(x))
-#limit(f(x), x=>Sym(pi)/4)
+y(n) = n == 1 ? 2 : sqrt(y(n-1)/2)
+sum([y(n) for n = 1:50])
 
-f(x) = x^3/(x-1)^2
-limit(f(x)/x, x=>oo)
-limit(f(x)-x, x=>oo)
+f(x) = exp(-cos(x)/(x-Sym(pi)/2))
+limit(f(x), x=>Sym(pi)/2)
+
+# h(x) = (x+1)(ln(x+1)-ln(x+2))
+# limit(h(x), x=>oo)
+# plot(h, 1, 10)
+
+g(x) = (2x)^(2/x)
+plot(g)
+N(solve(diff(g)))
